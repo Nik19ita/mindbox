3fd8e16d805154cfae078e4915b8833e3ccc07f3
Nikita <akter.kina@gmail.com>
2024-10-14 16:39:45 +0300

    ''

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
-  base: "/only/",
+  plugins: [react()],
+  base: "/mindbox/",
 })
