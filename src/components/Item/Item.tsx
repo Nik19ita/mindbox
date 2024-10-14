dbd7793190dbab3852ab4d9b612c1184f6da3bd1
Nikita <akter.kina@gmail.com>
2024-10-14 16:47:05 +0300

    ''

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -11,7 +11,7 @@ const Item: FC<IItemProps> = ({ el, toggleItem }) => {
   const { text, id, done } = el;
   return (
     <div className={styles.wraper} onClick={() => toggleItem(id)}>
-      <div className={styles.circul}>{done && <img src="../../../public/img/done.svg" />}</div>
+      <div className={styles.circul}>{done && <img src="img/done.svg" />}</div>
       <p
         className={[styles.paragraf, done === true && styles.completed].join(" ")} data-testid="item-paragraf"
       >
